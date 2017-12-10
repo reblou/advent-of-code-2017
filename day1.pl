fa294a0a36222efbbccc03022149f11379c020b7
reblou <rebeayto@gmail.com>
2017-12-10 17:56:53 +0000

    day 1 part 1 solution

--- day1.pl
+++ day1.pl
@@ -3,8 +3,27 @@
 use strict;
 use warnings;
 
-if ($ARGV[0] == 1) {
-    print "1", "\n";
-} else {
-    print "no" "\n";
+my $sum = 0;
+
+if(@ARGV) {
+    if ($ARGV[0] == 1) {
+        open(my $file, "<", "day1input");
+        my @lines = <$file>;
+        for my $line (@lines) {
+            my @chars = split //, $line;
+            my $prev = $chars[-2];
+            print "prev: ", $prev, "\n";
+            for my $char (@chars) {
+                unless ($char eq "\n") {
+                    if ($char eq $prev) {
+                        $sum += $prev;
+                    }
+                    $prev = $char;
+                }
+            }
+        }
+        print $sum, "\n";
+    } elsif ($ARGV[0] == 2) {
+
+    }
 }
