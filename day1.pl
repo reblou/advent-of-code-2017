6ed0d6c98cdf543745147246c2558e87e97f7a03
reblou <rebeayto@gmail.com>
2017-12-10 19:32:16 +0000

    cleaned up prints

--- day1.pl
+++ day1.pl
@@ -5,47 +5,46 @@ use warnings;
 
 my $sum = 0;
 
-if(@ARGV) {
-    if ($ARGV[0] == 1) {
-        open(my $file, "<", "day1input");
-        my @lines = <$file>;
-        for my $line (@lines) {
-            my @chars = split //, $line;
-            my $prev = $chars[-2];
-            for my $char (@chars) {
-                unless ($char eq "\n") {
-                    if ($char eq $prev) {
-                        $sum += $prev;
-                    }
-                    $prev = $char;
+unless (@ARGV) {
+    exit;
+}
+
+if ($ARGV[0] == 1) {
+    open(my $file, "<", "day1input");
+    my @lines = <$file>;
+    for my $line (@lines) {
+        my @chars = split //, $line;
+        my $prev = $chars[-2];
+        for my $char (@chars) {
+            unless ($char eq "\n") {
+                if ($char eq $prev) {
+                    $sum += $prev;
                 }
+                $prev = $char;
             }
         }
-        print $sum, "\n";
-    } elsif ($ARGV[0] == 2) {
-        open(my $file, "<", "day2input");
-        my @lines = <$file>;
-
-        for my $line (@lines) {
-            my @chars = split //, $line;
-            my $num_numbers = scalar(@chars) - 1;
-            my $step = $num_numbers / 2;
-
-            my $cmp_index;
-            for my $i (0 .. $num_numbers -1) {
-                $cmp_index = $i + $step;
-                if ($cmp_index > $num_numbers -1) {
-                    $cmp_index = $step - (($num_numbers-1) - $i) - 1;
-                    print "new cmp", $cmp_index, "\n";
-                }
-
-                if ($chars[$i] eq $chars[$cmp_index]) {
-
-                    print $chars[$i], "eq", $chars[$cmp_index], "\n";
-                    $sum += $chars[$i];
-                }
-            }
-        }
-        print "sum: ", $sum, "\n";
     }
+    print $sum, "\n";
+} elsif ($ARGV[0] == 2) {
+    open(my $file, "<", "day1input");
+    my @lines = <$file>;
+
+    for my $line (@lines) {
+        my @chars = split //, $line;
+        my $num_numbers = scalar(@chars) - 1;
+        my $step = $num_numbers / 2;
+
+        my $cmp_index;
+        for my $i (0 .. $num_numbers -1) {
+            $cmp_index = $i + $step;
+            if ($cmp_index > $num_numbers -1) {
+                $cmp_index = $step - (($num_numbers-1) - $i) - 1;
+            }
+
+            if ($chars[$i] eq $chars[$cmp_index]) {
+                $sum += $chars[$i];
+            }
+        }
+    }
+    print "sum: ", $sum, "\n";
 }
