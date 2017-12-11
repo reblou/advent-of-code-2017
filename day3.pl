0540194dfa21966a72c12c12d82f802b37b783e5
reblou <rebeayto@gmail.com>
2017-12-11 17:54:35 +0000

    day3 part one solution

---
+++ day3.pl
@@ -0,0 +1,102 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my $sum = 0;
+
+unless (@ARGV) {
+    exit;
+}
+
+#my $input = 265149;
+my $input = $ARGV[1];
+my @memory = ((0,0), (0,0));
+my $ring = 1;
+my $steps;
+
+
+if ($ARGV[0] == 1) {
+    my @ans = squares($input);
+
+    print "ans: \n";
+    foreach (@ans) {
+        print "$_, ";
+    }
+    print "\n";
+
+    my $rings = @ans;
+    #'print pop @ans,  "\n";
+    my @sol = getCoord(@ans, $rings, $input);
+    print "sol:\n";
+    foreach (@sol) {
+        print "$_, ";
+    }
+    print "\n";
+    $steps = distFromCoord(@sol);
+} elsif ($ARGV[0] == 2) {
+}
+
+print "steps: " . $steps . "\n";
+
+sub squares {
+    my $upto = shift;
+    my $square = 1;
+    my $i;
+    my @sqs;
+
+    do  {
+        $i = $square**2;
+        push @sqs, $i;
+        $square += 2;
+    } while ($i < $upto);
+
+    return @sqs;
+}
+
+sub sumUp {
+    my $x = shift;
+    my $i = 1;
+
+    while ($x > 1) {
+        $i += 2;
+        $x--;
+    }
+
+    return $i;
+}
+
+sub getCoord {
+    my @squares = @_;
+    my $x = pop @squares;
+    my $rings = pop @squares;
+    my $br = pop @squares;
+
+    if ($br == $x) {
+        my $c = -($rings-1);
+        return ($rings-1, $c);
+    }
+    my $ringstart = pop @squares;
+    my $blockDimensions = sumUp($rings);
+    my $dist = $x - $ringstart;
+    print "x: " . $x . "\n";
+    #my ($cx, $cy) = getCoord($ringstart, $rings-1, $ringstart);
+
+    if ($x <= ($ringstart + $blockDimensions - 1)) {
+        return ($rings-1, $dist-($rings-1));
+    } elsif ($x <= ($ringstart + 2*($blockDimensions - 1))) {
+        print "x: " . $x . ", rs: $ringstart, bd: $blockDimensions, d: $dist\n" ;
+
+        return (($rings - 1) - ($dist - $blockDimensions + 1), $rings-1);
+    } elsif ($x <= ($ringstart + 3*($blockDimensions - 1))) {
+        return (-($rings-1), -($dist-($rings-1)-2*($blockDimensions-1)));
+    } else {
+        return ( -((($rings - 1) - ($dist - 3*($blockDimensions - 1)))), 1-$rings)
+    }
+
+}
+
+sub distFromCoord {
+    my ($x, $y) = @_;
+    return (abs($x) + abs($y))
+
+}
