6b874f648df7f928a1c1ec9ddbce24e7dc3ac1db
reblou <rebeayto@gmail.com>
2017-12-26 21:49:05 +0000

    started day 14

---
+++ day14.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+unless (@ARGV) {
+    print "no arguments\n";
+    exit;
+}
+
+open(my $file, "<", "day14input");
+
+if ($ARGV[0] == 1) {
+    
+} elsif ($ARGV[0] == 2) {
+
+}
