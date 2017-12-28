5189819e8a2725fda65a0618e39b35a207afbe9d
reblou <rebeayto@gmail.com>
2017-12-28 22:41:28 +0000

    day 14 part 2 solution

--- day14.pl
+++ day14.pl
@@ -14,6 +14,8 @@ my @input = generateInput(256);
 open(my $file, "<", "day14input");
 my @lines = <$file>;
 my $total = 0;
+my @grid = [[]];
+my $groups = 0;
 
 if ($ARGV[0] == 1) {
     for my $i (0 .. 127) {
@@ -33,6 +35,188 @@ if ($ARGV[0] == 1) {
 
     print "total squares: $total\n";
 } elsif ($ARGV[0] == 2) {
+    # get
+    for my $i (0 .. 127) {
+        my $str = $lines[0];
+        $str =~ s/\s+$//g;
+        $str .= "-" . $i;
+        #print "string: $str\n";
+        my @codes = getASCIIcodes($str);
+        my @endSequence = (17, 31, 73, 47, 23);
+        push @codes, @endSequence;
+        my @a = nRounds(64, @codes);
+        my @dense = densify(@a);
+        toHexOutput(@dense);
+        my $bin = toBinary(@dense);
+        #getRow($bin, $i);
+        #printRow($bin);
+        print "filling grid\n";
+        fillGrid($i, $bin);
+    }
+    printGrid();
+    #print "groups: $groups\n";
+    #getNumGroups();
+    #printEx();
+    getRegions();
+
+} elsif ($ARGV[0] == 3) {
+    my @rows = ("11010100", "01010101", "00001010",
+    "10101101", "01101000", "11001001", "01000100", "11010110");
+    for my $i (0 .. $#rows) {
+        #getRow($rows[$i], $i);
+        #@{$grid[$i]} = $rows[$i];
+        fillGrid($i, $rows[$i]);
+    }
+    printGrid();
+    #print "groups: $groups\n";
+    #getNumGroups();
+    getRegions();
+
+}
+
+sub getRegions {
+    my $regions = 0;
+
+    for my $n (0 .. $#grid) {
+        foreach my $i (0 .. $#{$grid[$n]}) {
+            if ($grid[$n][$i]) {
+                print "removing from $n, $i:\n";
+                removeRegion($n, $i);
+                #printGrid();
+                $regions++;
+            }
+        }
+    }
+    print "regions: $regions\n";
+}
+
+sub removeRegion {
+    my ($n, $i) = @_;
+    unless ($grid[$n][$i]) {
+        return;
+    }
+    #print "-n: $n, i: $i\n";
+    $grid[$n][$i] = 0;
+    removeRegion($n+1, $i) if $n+1 < @grid;
+    removeRegion($n-1, $i) if $n-1 >= 0;
+    removeRegion($n, $i-1) if $i-1 >= 0;
+    removeRegion($n, $i+1) if $i+1 < @{$grid[$n]};
+
+
+}
+
+sub fillGrid {
+    my ($row, $str) = @_;
+    my @arr = split //, $str;
+    for my $i (0 .. $#arr) {
+        $grid[$row][$i] = $arr[$i];
+    }
+}
+
+sub getNumGroups {
+    my %nums;
+
+    foreach my $n (0 .. 7) {
+        foreach my $i (0 .. 7) {
+            $nums{$grid[$n][$i]} = 1;
+        }
+    }
+
+    my @k = keys %nums;
+    print "groups: @k\n";
+    print "number of different groups: ". (@k-1) . "\n";
+}
+
+sub printEx {
+    print "--\n";
+    for my $n (0 .. 7) {
+        print "|";
+        for my $i (0 .. 7) {
+            print $grid[$n][$i];
+        }
+        print "|\n";
+    }
+    print "--\n";
+}
+
+sub printGrid {
+    print "--";
+    for my $x (0 .. $#grid) {
+        print "\n|";
+        foreach my $y (0 .. $#{$grid[$x]}) {
+            if($grid[$x][$y]) {
+                print "#";
+            } else {
+                print ".";
+            }
+        }
+        print "|";
+    }
+    print "\n--\n";
+}
+
+sub getRow {
+    my ($binStr, $n) = @_;
+    my @a = split //, $binStr;
+    for my $i (0 .. $#a) {
+        if ($a[$i] eq "0") {
+            $grid[$n][$i] = ".";
+        } elsif ($a[$i] eq "1") {
+            #push @row, "#";
+            #getGroup($i, $n, @a);
+            if ($n > 0 && $grid[$n-1][$i] ne ".") {
+                my $back = 1;
+                #while ($i-$back >= 0 && $grid[$n][$i-$back] ne ".") {
+                    #pop @row;
+                    #push @row, $grid[$n][$i-1];
+                    #$grid[$n][$i-$back] = $grid[$n-1][$i];
+                    #$back++;
+                #}
+                #push @row, $grid[$n-1][$i];
+                $grid[$n][$i] = $grid[$n-1][$i];
+            } elsif ($i > 0 && $grid[$n][$i-1] ne ".") {
+                #push @row, $grid[$n][$i];
+                $grid[$n][$i] = $grid[$n][$i-1];
+            } else {
+                #push @row, $groups++;
+                $grid[$n][$i] = ++$groups;
+            }
+            backtrace($n, $i);
+        } else {
+            print "oh dear\n";
+            exit();
+        }
+    }
+}
+
+sub backtrace {
+    my ($n, $i) = @_;
+    if ($grid[$n][$i] eq ".") {
+        print "abort\n";
+        exit();
+    }
+    my $groupnum = $grid[$n][$i];
+    #print "backtracing for: $groupnum, at n: $n, i: $i\n";
+    if ($n > 0 && $grid[$n-1][$i] ne ".") {
+        $grid[$n-1][$i] = $groupnum;
+        backtrace($n-1, $i);
+    }
+
+    my $pos = $i;
+    while ($grid[$n][++$pos]) {
+        if ($grid[$n][$i] ne ".") {
+            $grid[$n][$i] = $groupnum;
+        }
+
+    }
+
+    while ($i > 0 && $grid[$n][--$i] ne ".") {
+        $grid[$n][$i] = $groupnum;
+        if ($n > 0 && $grid[$n-1][$i] ne ".") {
+            $grid[$n-1][$i] = $groupnum;
+            backtrace($n-1, $i);
+        }
+    }
 
 }
 
